5de7513625038cbda04102628db162b54c3d9b36
MNaveenReddy123 <mnaveenreddy.2004@gmail.com>
2025-04-12 10:14:10 +0530

    final

--- prepare_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import T5Tokenizer
-
-# Load tokenizer from HuggingFace
-tokenizer = T5Tokenizer.from_pretrained("t5-large")
-
-# Save tokenizer files to your folder
-tokenizer.save_pretrained("./t5_finetuned_qa_large")
-
-print("Tokenizer files added successfully.")
